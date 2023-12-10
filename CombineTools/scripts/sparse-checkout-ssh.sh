d75fcd2ad33a8574f14be42b776696f296c9db5c
Amitav Mitra <64038220+ammitra@users.noreply.github.com>
2023-12-10 11:22:56 -0500

    point to JHU-Tools repository for posterity
    removes need for dealing with ssh keys in Gitlab CI/CD

--- CombineTools/scripts/sparse-checkout-ssh.sh
+++ CombineTools/scripts/sparse-checkout-ssh.sh
@@ -8,7 +8,7 @@ else
   pushd $CMSSW_BASE/src
   mkdir CombineHarvester; cd CombineHarvester
   git init
-  git remote add origin git@github.com:lcorcodilos/CombineHarvester.git
+  git remote add origin https://github.com/JHU-Tools/CombineHarvester.git
   git config core.sparsecheckout true; echo CombineTools/ >> .git/info/sparse-checkout
   git pull origin master
   popd
